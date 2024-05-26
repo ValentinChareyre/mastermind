5e078ea1b5d35e8e0cb0279832659d4573374d9f
Leilane Cambará <leilane.cambara@gmail.com>
2024-05-26 16:07:59 +0100

    Move CodeMaster class and add export

--- src/CodeMaster.js
+++ src/CodeMaster.js
@@ -1,27 +1,26 @@
 // This class has two methods:
-//      - generateSecretCode, which requires a length (integer) to randomly create a Code array, named secretCode
+//      - generateSecretCode, which requires *length* (number of pegs in a code) and *colours* (number of colours options to choose from) to randomly create a Code array, named secretCode
 //      - checkCode, which compares an input given by the player (userCode) with the secretCode and returns testResult, an array of integers of the same length as userCode and secretCode:
 //              0 for when the colour is not in the secretCode;
 //              1 for when the colour is in the secretCode and is in the same position;
 //              2 for when the colour is in the secretCode, but in a different position.
 
-class CodeMaster {
-
-    generateSecretCode(length) {
+export class CodeMaster {
+    generateSecretCode(length, colours) {
         let secretCode = new Code(length);
         for (let i = 0; i < length; i++) {
-            secretCode[i] = Math.floor(Math.random()*length);
+            secretCode[i] = Math.floor(Math.random()*colours);
         }
         return secretCode;
     }
 
-    checkCode(userCode) {
+    checkCode(userCode, secretCode) {
         const n = userCode.length;
         let testResult = new Array(n);
 
         for (let i = 0; i < n; i++) {
             if (secretCode.includes(userCode[i])) {
-                if (userCode[i] == secretCode[i]) {
+                if (userCode[i] === secretCode[i]) {
                     testResult[i] = 1;
                 } else {
                     testResult[i] = 2;
@@ -33,4 +32,5 @@ class CodeMaster {
 
         return testResult;
     }
+
 }
